84200806b5cb4c6345d6256b294c24ef1fbcaa1a
pranavi <vachanta@uncc.edu>
2020-10-15 09:50:09 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <article className = "hero">
+            <h1>Personal Budget</h1>
+            <h2>A personal-budget management app</h2>
+    </article>
+  );
+}
+
+export default Hero;
